bf8159804bb750ce291bf26d2a4678df5a8b22e7
Helson Secretaria <helsonsecretaria@gmail.com>
2020-03-16 18:53:16 +0800

    Commit Service and Repository Interfaces, CourseViewModels

---
+++ CleanArchitecture/CleanArchitecture.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,13 @@
+﻿
+using CleanArchitecture.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}
